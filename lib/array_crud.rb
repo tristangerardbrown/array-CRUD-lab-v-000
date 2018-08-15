05dfd2883a17e37ab8bf5c98b1a183282a3a0518
Tristan Brown <tgbrown@stanford.edu>
2018-08-15 22:30:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,12 +36,12 @@ birds_array[index_number]
 
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array(birds_array)
 
 birds_array[0]
 end
 
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array(birds_array)
 
 birds_array[-1]
 end
